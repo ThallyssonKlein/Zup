3140bf40b47eceddb41589f27905ae780b7efacd
Thallysson Klein <thallyssonklein@gmail.com>
2021-07-08 08:10:29 +0000

    Conta Corrente

--- conta_corrente.py
+++ conta_corrente.py
@@ -1,10 +1,10 @@
-import uuid
+import minidb
 
-class ContaCorrente:
+class ContaCorrente(minidb.Model):
+    saldo = float
+
     def __init__(self):
         self.saldo = 0
-        self.endereco = uuid.uuid4().hex
-
     
     def consultar_saldo(self):
         return self.saldo
@@ -19,10 +19,19 @@ class ContaCorrente:
     def receber_transferencia(self, quantidade):
         self.saldo += quantidade
     
-    def transferencias(self, quantidade, outra_conta):
+    def transferir(self, quantidade, outra_conta):
         if self.saldo - quantidade >= 0:
             self.saldo -= quantidade
             outra_conta.receber_transferencia(quantidade)
     
-
-        
+if __name__ == "__main__":
+    db = minidb.Store('database.db', debug=True)
+    db.register(ContaCorrente)
+    conta1 = ContaCorrente()
+    conta1.depositar(100)
+    conta1.sacar(10)
+    print(conta1.consultar_saldo())
+    conta2 = ContaCorrente()
+    conta1.transferir(10, conta2)
+    print(conta1.consultar_saldo())
+    print(conta2.consultar_saldo())
